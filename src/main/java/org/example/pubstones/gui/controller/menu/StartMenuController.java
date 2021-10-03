be0463c0f40ecdb104383c3fedb05be1da148203
DustinScharf <d.scharf.2002@gmail.com>
2021-10-04 01:06:10 +0200

    Added onAction methods for buttons

--- src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
+++ src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
@@ -1,4 +1,14 @@
 package org.example.pubstones.gui.controller.menu;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class StartMenuController {
+    @FXML
+    public void playButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void exitButtonClicked(ActionEvent actionEvent) {
+    }
 }
