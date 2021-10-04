4ad529f4dda39c35b63057891dbd8897de7572b9
DustinScharf <d.scharf.2002@gmail.com>
2021-10-04 02:36:24 +0200

    Made every controller extend the base controller

--- src/main/java/org/example/pubstones/gui/controller/game/GameController.java
+++ src/main/java/org/example/pubstones/gui/controller/game/GameController.java
@@ -2,8 +2,9 @@ package org.example.pubstones.gui.controller.game;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import org.example.pubstones.gui.controller.BaseController;
 
-public class GameController {
+public class GameController extends BaseController {
     @FXML
     public void backButtonClicked(ActionEvent actionEvent) {
     }
--- src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
+++ src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
@@ -2,8 +2,9 @@ package org.example.pubstones.gui.controller.menu;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import org.example.pubstones.gui.controller.BaseController;
 
-public class StartMenuController {
+public class StartMenuController extends BaseController {
     @FXML
     public void playButtonClicked(ActionEvent actionEvent) {
     }
