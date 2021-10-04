4f19ab5898a7e684d6732017f790d61e9d394929
DustinScharf <d.scharf.2002@gmail.com>
2021-10-04 03:38:25 +0200

    Implemented button action listeners

--- src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
+++ src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
@@ -3,13 +3,16 @@ package org.example.pubstones.gui.controller.menu;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import org.example.pubstones.gui.controller.BaseController;
+import org.example.pubstones.util.datatyp.historylist.OutOfTimelineException;
 
 public class StartMenuController extends BaseController {
     @FXML
     public void playButtonClicked(ActionEvent actionEvent) {
+        super.sceneManager.switchScene("/fxml/menu/Game.fxml");
     }
 
     @FXML
     public void exitButtonClicked(ActionEvent actionEvent) {
+        super.sceneManager.getStage().close();
     }
 }
