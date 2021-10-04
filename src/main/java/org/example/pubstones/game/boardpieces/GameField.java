ef910454320ba7f865ca39440531b1f57e87e9de
Vincent Putzke <vincentputzke@gmx.de>
2021-10-04 19:09:56 +0200

    Added first player interaction functions

--- src/main/java/org/example/pubstones/game/boardpieces/GameField.java
+++ src/main/java/org/example/pubstones/game/boardpieces/GameField.java
@@ -9,5 +9,18 @@ public class GameField {
         
     }
     
+    public boolean tryPlaceStone(Symbol symbol, int index) {
+        if (!this.stonePile.contains(symbol)) {
+            return false;
+        }
+        if (this.stoneLine.isFull()) {
+            return false;
+        }
+        return stoneLine.placeStone(stonePile.takeStone(symbol), index);
+    }
+    
+    public boolean trySwapStones(int index1, int index2) {
+        return stoneLine.swapStones(index1, index2);
+    }
     
 }
