687098b63d44fd56c8afe9633daa0d0e84213f27
DustinScharf <d.scharf.2002@gmail.com>
2021-10-04 03:36:11 +0200

    Added own launcher class

---
+++ src/main/java/org/example/pubstones/launcher/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.pubstones.launcher;
+
+import org.example.pubstones.gui.launcher.PubStonesApp;
+
+public class Launcher {
+    public static void main(String[] args) {
+        PubStonesApp.main(args);
+    }
+}
