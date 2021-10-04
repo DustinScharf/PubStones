1f07481171c23d190b163496a2dc803252a2e8c0
DustinScharf <d.scharf.2002@gmail.com>
2021-10-04 03:42:58 +0200

    Implemented button action listeners

--- src/main/java/org/example/pubstones/gui/controller/game/GameController.java
+++ src/main/java/org/example/pubstones/gui/controller/game/GameController.java
@@ -3,9 +3,15 @@ package org.example.pubstones.gui.controller.game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import org.example.pubstones.gui.controller.BaseController;
+import org.example.pubstones.util.datatyp.historylist.OutOfTimelineException;
 
 public class GameController extends BaseController {
     @FXML
     public void backButtonClicked(ActionEvent actionEvent) {
+        try {
+            super.sceneManager.switchToPreviousScene();
+        } catch (OutOfTimelineException e) {
+            e.printStackTrace();
+        }
     }
 }
