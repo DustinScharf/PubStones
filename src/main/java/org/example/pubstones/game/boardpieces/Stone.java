8e1f8485ca2301ecffd726a9e6e46ca1cbdc89f8
Vincent Putzke <vincentputzke@gmx.de>
2021-10-04 11:17:43 +0200

    base stone class added; base functions

---
+++ src/main/java/org/example/pubstones/game/boardpieces/Stone.java
@@ -0,0 +1,77 @@
+package org.example.pubstones.game.boardpieces;
+
+public class Stone {
+    
+    private Symbol symbol;
+    private boolean turned = false;
+    private boolean onBoard = false;
+    
+    /**
+     * Creates a new stone with the given symbol
+     * @param symbol the stone's symbol
+     */
+    public Stone(Symbol symbol) {
+        this.setSymbol(symbol);
+    }
+    
+    /**
+     * This stone's symbol
+     * @return 
+     */
+    public Symbol getSymbol() {
+        return this.symbol;
+    }
+    
+    /**
+     * Sets a new symbol
+     * @param symbol new symbol
+     */
+    public void setSymbol(Symbol symbol) {
+        this.symbol = symbol;
+    }
+    
+    /**
+     * This stone's turn state
+     * @return
+     */
+    public boolean isTurned() {
+        return this.turned;
+    }
+    
+    /**
+     * Sets a new turn state
+     * @param turned
+     */
+    public void setTurned(boolean turned) {
+        this.turned = turned;
+    }
+    
+    /**
+     * Turns this stone
+     */
+    public void turn() {
+        this.turned = !this.turned;
+    }
+    
+    /**
+     * This stone's on board state
+     * @return
+     */
+    public boolean isOnBoard() {
+        return this.onBoard;
+    }
+    
+    /**
+     * Places this stone on the board
+     */
+    public void placeOnBoard() {
+        this.onBoard = true;
+    }
+    
+    /**
+     * Symbol indicator for stones
+     */
+    private enum Symbol {
+        A, B, C, D, E, F, G;
+    }
+}
