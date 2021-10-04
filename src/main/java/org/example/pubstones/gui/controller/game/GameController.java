f43c0334d3a0d4b225cd193cf80099ef63819d9b
DustinScharf <d.scharf.2002@gmail.com>
2021-10-04 02:35:45 +0200

    Added back button action listener

--- src/main/java/org/example/pubstones/gui/controller/game/GameController.java
+++ src/main/java/org/example/pubstones/gui/controller/game/GameController.java
@@ -1,4 +1,10 @@
 package org.example.pubstones.gui.controller.game;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class GameController {
+    @FXML
+    public void backButtonClicked(ActionEvent actionEvent) {
+    }
 }
