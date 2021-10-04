17d5a55a87e3acf24395bd52da182d8e1441b033
DustinScharf <d.scharf.2002@gmail.com>
2021-10-04 03:22:52 +0200

    Added OutOfTimelineException

---
+++ src/main/java/org/example/pubstones/util/datatyp/historylist/OutOfTimelineException.java
@@ -0,0 +1,7 @@
+package org.example.pubstones.util.datatyp.historylist;
+
+public class OutOfTimelineException extends Exception {
+    public OutOfTimelineException(String message) {
+        super(message);
+    }
+}
