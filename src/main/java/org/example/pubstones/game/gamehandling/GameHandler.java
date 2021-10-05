36aa001c90d5e653627376fc3661dcc63894c5bf
Vincent Putzke <vincentputzke@gmx.de>
2021-10-05 23:18:13 +0200

    Added game handler player queue

--- src/main/java/org/example/pubstones/game/gamehandling/GameHandler.java
+++ src/main/java/org/example/pubstones/game/gamehandling/GameHandler.java
@@ -4,13 +4,31 @@ import java.util.ArrayList;
 
 import org.example.pubstones.game.boardpieces.GameField;
 import org.example.pubstones.game.gamehandling.gamemoves.*;
+import org.example.pubstones.util.datatype.Queue;
 
 public class GameHandler {
     
     private ArrayList<GamePlayer> players;
     private MoveHistory moveHistory;
     
+    private Queue<GamePlayer> playerQueue;
+    
     private GameField gameField;
+    
+    public GameHandler() {
+        this.players = new ArrayList<GamePlayer>();
+        this.playerQueue = new Queue<GamePlayer>();
+    }
+    
+    public void addPlayer(GamePlayer gamePlayer) {
+        this.players.add(gamePlayer);
+        this.playerQueue.enqueue(gamePlayer);
+    }
+    
+    public void removePlayer(GamePlayer gamePlayer) {
+        this.players.remove(gamePlayer);
+        this.playerQueue.remove(gamePlayer);
+    }
     
     public GameField getCurrentState() {
         return this.gameField;
@@ -19,6 +37,16 @@ public class GameHandler {
     public void receiveGameMove(GameMove gameMove) {
         gameMove.applyMove(this.gameField);
         moveHistory.add(gameMove);
+    }
+    
+    public GamePlayer nextPlayer() {
+        GamePlayer currentLastPlayer = this.playerQueue.dequeue();
+        this.playerQueue.enqueue(currentLastPlayer);
+        return this.playerQueue.first();
+    }
+    
+    public GamePlayer getCurrentPlayer() {
+        return this.playerQueue.first();
     }
     
 }
