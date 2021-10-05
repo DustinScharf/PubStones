a4a90d74b8fe05b6f5f270140b1f61e9f362b40c
Vincent Putzke <vincentputzke@gmx.de>
2021-10-05 13:31:53 +0200

    Initial move history class

---
+++ src/main/java/org/example/pubstones/game/gamehandling/MoveHistory.java
@@ -0,0 +1,48 @@
+package org.example.pubstones.game.gamehandling;
+
+import java.util.ArrayList;
+
+public class MoveHistory {
+    
+    private ArrayList<GameMove> moves;
+    
+    /**
+     * Creates a new move history
+     */
+    public MoveHistory() {
+        this.moves = new ArrayList<GameMove>();
+    }
+    
+    /**
+     * This history's move count
+     * @return
+     */
+    public int getMoveCount() {
+        return this.moves.size();
+    }
+    
+    /**
+     * Adds the given move to the history
+     * @param move
+     */
+    public void add(GameMove move) {
+        this.moves.add(move);
+    }
+    
+    /**
+     * Deletes the last move and returns it
+     * @return
+     */
+    public GameMove undo() {
+        return this.moves.remove(this.moves.size() - 1);
+    }
+    
+    /**
+     * The last move in this history
+     * @return
+     */
+    public GameMove last() {
+        return this.moves.get(this.moves.size() - 1);
+    }
+    
+}
