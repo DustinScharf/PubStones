45a10db1c1d25895ea5a1f05e0fef86d9e7572aa
Vincent Putzke <vincentputzke@gmx.de>
2021-10-05 23:13:06 +0200

    Added remove method; Added comments

--- src/main/java/org/example/pubstones/util/datatype/Queue.java
+++ src/main/java/org/example/pubstones/util/datatype/Queue.java
@@ -6,24 +6,51 @@ public class Queue<T> {
     
     private LinkedList<T> list;
     
+    /**
+     * Creates a new queue object
+     */
     public Queue() {
         this.list = new LinkedList<T>();
     }
     
+    /**
+     * Enqueues the given object 
+     * @param object
+     */
     public void enqueue(T object) {
         this.list.add(object);
     }
     
+    /**
+     * Dequeues the first element
+     * @return
+     */
     public T dequeue() {
         return this.list.removeFirst();
     }
     
+    /**
+     * This queue's size
+     * @return
+     */
     public int size() {
         return this.list.size();
     }
     
+    /**
+     * Checks whether the queue is empty
+     * @return
+     */
     public boolean isEmpty() {
         return this.list.isEmpty();
+    }
+    
+    /**
+     * Removes the given object from the queue
+     * @param object
+     */
+    public void remove(T object) {
+        this.list.remove(object);
     }
     
 }
