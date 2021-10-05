2dab62174b65c6ebc47196c25c72e0bce008e0fb
DustinScharf <d.scharf.2002@gmail.com>
2021-10-06 01:21:56 +0200

    Created initialize for gameHandler

--- src/main/java/org/example/pubstones/gui/controller/game/GameController.java
+++ src/main/java/org/example/pubstones/gui/controller/game/GameController.java
@@ -2,10 +2,25 @@ package org.example.pubstones.gui.controller.game;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.example.pubstones.game.gamehandling.GameHandler;
+import org.example.pubstones.game.gamehandling.GamePlayer;
 import org.example.pubstones.gui.controller.BaseController;
 import org.example.pubstones.util.exception.OutOfTimelineException;
 
-public class GameController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameController extends BaseController implements Initializable {
+    private GameHandler gameHandler;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.gameHandler = new GameHandler();
+        this.gameHandler.addPlayer(new GamePlayer("Player 1"));
+        this.gameHandler.addPlayer(new GamePlayer("Player 2"));
+    }
+
     @FXML
     public void backButtonClicked(ActionEvent actionEvent) {
         try {
