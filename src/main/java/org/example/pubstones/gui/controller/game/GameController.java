44b8d001e1741ccb38448493bb64612a25fc036c
DustinScharf <d.scharf.2002@gmail.com>
2021-10-07 02:56:27 +0200

    Added updateGuiToCurrentGameState method

--- src/main/java/org/example/pubstones/gui/controller/game/GameController.java
+++ src/main/java/org/example/pubstones/gui/controller/game/GameController.java
@@ -8,7 +8,12 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import org.example.pubstones.game.boardpieces.Stone;
+import org.example.pubstones.game.boardpieces.StonePile;
 import org.example.pubstones.game.boardpieces.Symbol;
+import org.example.pubstones.game.boardpieces.exceptions.StoneLineFullException;
+import org.example.pubstones.game.boardpieces.exceptions.StoneNotFoundException;
+import org.example.pubstones.game.boardpieces.exceptions.StonesEqualException;
 import org.example.pubstones.game.gamehandling.GameHandler;
 import org.example.pubstones.game.gamehandling.GameMove;
 import org.example.pubstones.game.gamehandling.GamePlayer;
@@ -18,6 +23,7 @@ import org.example.pubstones.gui.controller.BaseController;
 import org.example.pubstones.util.exception.OutOfTimelineException;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class GameController extends BaseController implements Initializable {
@@ -62,6 +68,76 @@ public class GameController extends BaseController implements Initializable {
             this.symbolsHBox.getChildren().add(symbolButton);
         }
 
+//        int stonePileSize = this.gameHandler.getCurrentState().getStonePile().getStones().size();
+//        for (int i = 0; i < stonePileSize; ++i) {
+//            Button stonePileButton = new Button(
+//                    "" + this.gameHandler.getCurrentState().getStonePile().getStones().get(i)
+//            );
+//            this.stonePileHBox.getChildren().add(stonePileButton);
+//        }
+
+//        int playerCount = this.gameHandler.getPlayers().size();
+//        for (int i = 0; i < playerCount; ++i) {
+//            GamePlayer currentPlayer = this.gameHandler.getPlayers().get(i);
+//            Label playerScoreLabel = new Label(currentPlayer.getName() + " : " + currentPlayer.getScore());
+//            this.scoresVBox.getChildren().add(playerScoreLabel);
+//        }
+
+//        this.currentPlayerLabel.setText("Current Player: " + this.gameHandler.getCurrentPlayer().getName());
+
+//        this.winnerLabel.setText(
+//                "Winner: " + (this.gameHandler.isGameOver() ? this.gameHandler.getLeadingPlayer().getName() : "none")
+//        );
+
+//        int playerActionsCount = this.gameHandler.getCurrentPlayer().getPossibleMoves().length;
+//        for (int i = 0; i < playerActionsCount; ++i) {
+//            Button playerActionButton = new Button("" + this.gameHandler.getCurrentPlayer().getPossibleMoves()[i]);
+//            this.playerActionsHBox.getChildren().add(playerActionButton);
+//        }
+
+        // TODO extract to game logic
+        try {
+            int stonePileSize = this.gameHandler.getCurrentState().getStonePile().getStones().size();
+            StonePile stonePile = this.gameHandler.getCurrentState().getStonePile();
+            Stone stoneToPlace = stonePile.getStones().get(new Random().nextInt(stonePileSize));
+
+            GameMove gameMove = GameMove.getMove(MoveKind.Place).index(0).stone(stoneToPlace);
+            this.gameHandler.receiveGameMove(gameMove);
+        } catch (IllegalMoveArgumentException |
+                StonesEqualException |
+                StoneLineFullException |
+                StoneNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        this.updateGuiToCurrentGameState();
+    }
+
+    private void updateGuiToCurrentGameState() {
+        //
+        // Update the stone line display
+        //
+        int stoneLineSize = this.gameHandler.getCurrentState().getStoneLine().getStones().size();
+        for (int i = 0; i < stoneLineSize; ++i) {
+            Button stoneLineButton = new Button(
+                    "" + this.gameHandler.getCurrentState().getStoneLine().getStones().get(i)
+            );
+            this.stoneLineHBox.getChildren().add(stoneLineButton);
+        }
+
+        //
+        // Update the score display of all players
+        //
+        int playerCount = this.gameHandler.getPlayers().size();
+        for (int i = 0; i < playerCount; ++i) {
+            GamePlayer currentPlayer = this.gameHandler.getPlayers().get(i);
+            Label playerScoreLabel = new Label(currentPlayer.getName() + " : " + currentPlayer.getScore());
+            this.scoresVBox.getChildren().add(playerScoreLabel);
+        }
+
+        //
+        // Updates the stone pile display
+        //
         int stonePileSize = this.gameHandler.getCurrentState().getStonePile().getStones().size();
         for (int i = 0; i < stonePileSize; ++i) {
             Button stonePileButton = new Button(
@@ -70,24 +146,25 @@ public class GameController extends BaseController implements Initializable {
             this.stonePileHBox.getChildren().add(stonePileButton);
         }
 
-        int playerCount = this.gameHandler.getPlayers().size();
-        for (int i = 0; i < playerCount; ++i) {
-            GamePlayer currentPlayer = this.gameHandler.getPlayers().get(i);
-            Label playerScoreLabel = new Label(currentPlayer.getName() + " : " + currentPlayer.getScore());
-            this.scoresVBox.getChildren().add(playerScoreLabel);
-        }
-
-        this.currentPlayerLabel.setText("Current Player: " + this.gameHandler.getCurrentPlayer().getName());
-
-        this.winnerLabel.setText(
-                "Winner: " + (this.gameHandler.isGameOver() ? this.gameHandler.getLeadingPlayer().getName() : "none")
-        );
-
+        //
+        // Updates the possible player actions display
+        //
         int playerActionsCount = this.gameHandler.getCurrentPlayer().getPossibleMoves().length;
         for (int i = 0; i < playerActionsCount; ++i) {
             Button playerActionButton = new Button("" + this.gameHandler.getCurrentPlayer().getPossibleMoves()[i]);
             this.playerActionsHBox.getChildren().add(playerActionButton);
         }
+
+        //
+        // Updates the current player display
+        //
+        this.currentPlayerLabel.setText("Current Player: " + this.gameHandler.getCurrentPlayer().getName());
+
+        //
+        // Updates the winner display
+        //
+        String winnerInfo = this.gameHandler.isGameOver() ? this.gameHandler.getLeadingPlayer().getName() : "none";
+        this.winnerLabel.setText("Winner: " + winnerInfo);
     }
 
     @FXML
