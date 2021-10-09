d156a980e3c76da98710610e026d50e24a7fccae
DustinScharf <d.scharf.2002@gmail.com>
2021-10-09 23:02:37 +0200

    Added mute options

--- src/main/java/org/example/pubstones/gui/manager/MusicManager.java
+++ src/main/java/org/example/pubstones/gui/manager/MusicManager.java
@@ -19,8 +19,21 @@ public class MusicManager {
 
     private final Map<String, Media> music;
 
+    private double lastVolume = 1; // MAX VOLUME
+
+    private boolean isPlaying = false;
+    private boolean isMuted = false;
+
     public MusicManager() {
         this.music = new HashMap<>();
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    public boolean isMuted() {
+        return isMuted;
     }
 
     public void setMusic(String mp3FilePath) {
@@ -35,23 +48,64 @@ public class MusicManager {
 //        });
         Media musicMedia = new Media(getClass().getResource(mp3FilePath).toString());
         this.mediaPlayer = new MediaPlayer(musicMedia);
+        this.mediaPlayer.setVolume(this.isMuted ? 0 : this.lastVolume);
+        this.mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
     }
 
     public void playMusic(int fadeInSeconds) {
-        this.mediaPlayer.setVolume(0);
-        this.mediaPlayer.play();
-        Timeline timeline = new Timeline(
-                new KeyFrame(Duration.seconds(fadeInSeconds),
-                        new KeyValue(this.mediaPlayer.volumeProperty(), 1)));
-        timeline.play();
+        if (this.isPlaying) {
+            return;
+        }
+
+        if (fadeInSeconds > 0 && !this.isMuted) {
+            this.mediaPlayer.setVolume(0);
+            this.mediaPlayer.play();
+            Timeline timeline = new Timeline(
+                    new KeyFrame(Duration.seconds(fadeInSeconds),
+                            new KeyValue(this.mediaPlayer.volumeProperty(), 1)));
+            timeline.play();
+        }
+        this.isPlaying = true;
     }
 
     public void stopMusic(int fadeOutSeconds) {
+        if (!this.isPlaying) {
+            return;
+        }
+
         DelayRunner.startSleeper(fadeOutSeconds).setOnSucceeded(event -> this.mediaPlayer.stop());
 
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.seconds(fadeOutSeconds),
                         new KeyValue(this.mediaPlayer.volumeProperty(), 0)));
         timeline.play();
+    }
+
+    public void muteMusic(int fadeOutSeconds) {
+        if (this.isMuted) {
+            return;
+        }
+
+        this.lastVolume = this.mediaPlayer.getVolume();
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(fadeOutSeconds),
+                        new KeyValue(this.mediaPlayer.volumeProperty(), 0)));
+        timeline.play();
+
+        this.isMuted = true;
+    }
+
+    public void unMuteMusic(int fadeInSeconds) {
+        if (!this.isMuted) {
+            return;
+        }
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(fadeInSeconds),
+                        new KeyValue(this.mediaPlayer.volumeProperty(), this.lastVolume)));
+        timeline.play();
+
+        this.isMuted = false;
     }
 }
