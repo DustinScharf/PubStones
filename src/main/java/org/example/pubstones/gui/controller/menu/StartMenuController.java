675c4998e27e9392c0e9f6f822768d937c45ccff
DustinScharf <d.scharf.2002@gmail.com>
2021-10-09 23:02:48 +0200

    Added mute hotkey

--- src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
+++ src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
@@ -6,6 +6,10 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
@@ -17,6 +21,18 @@ import java.util.ResourceBundle;
 public class StartMenuController extends BaseController {
     @Override
     public void init() {
+        KeyCombination controlMKeyCombination = new KeyCodeCombination(KeyCode.M, KeyCodeCombination.CONTROL_DOWN);
+        super.sceneManager.getStage().getScene().addEventFilter(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+            if(controlMKeyCombination.match(event)){
+                if (super.musicManager.isMuted()) {
+                    super.musicManager.unMuteMusic(3);
+                } else {
+                    super.musicManager.muteMusic(1);
+                }
+            }
+            event.consume();
+        });
+
         super.musicManager.setMusic("/music/menu/1.mp3");
         super.musicManager.playMusic(3);
     }
