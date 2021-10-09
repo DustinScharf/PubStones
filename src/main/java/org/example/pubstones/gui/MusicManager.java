1e10ce87eee471e66723da21a7b22cc99bf0424d
DustinScharf <d.scharf.2002@gmail.com>
2021-10-09 05:10:18 +0200

    Implemented MusicManager

---
+++ src/main/java/org/example/pubstones/gui/MusicManager.java
@@ -0,0 +1,74 @@
+package org.example.pubstones.gui;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+import org.example.pubstones.gui.controller.BaseController;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MusicManager {
+    private MediaPlayer mediaPlayer;
+
+    private final Map<String, Media> music;
+
+    public MusicManager() {
+        this.music = new HashMap<>();
+    }
+
+    public void setMusic(String mp3FilePath) {
+        Media musicMedia = this.music.computeIfAbsent(mp3FilePath, mp3FilePathTemp -> {
+            URL url = getClass().getResource(mp3FilePathTemp);
+            if (url == null) {
+                throw new NullPointerException(); // TODO write own exception
+            }
+
+            String path = url.toString();
+            return new Media(path);
+        });
+        this.mediaPlayer = new MediaPlayer(musicMedia);
+    }
+
+    public void playMusic(int fadeInSeconds) {
+        this.mediaPlayer.setVolume(0);
+        this.mediaPlayer.play();
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(fadeInSeconds),
+                        new KeyValue(this.mediaPlayer.volumeProperty(), 1)));
+        timeline.play();
+    }
+
+    public void stopMusic(int fadeInSeconds) {
+        // TODO extract basic sleeper structure to other class
+        Task<Void> sleeper = new Task<>() {
+            @Override
+            protected Void call() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ignored) {
+                }
+                return null;
+            }
+        };
+        sleeper.setOnSucceeded(event -> this.mediaPlayer.stop());
+        new Thread(sleeper).start();
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(fadeInSeconds),
+                        new KeyValue(this.mediaPlayer.volumeProperty(), 0)));
+        timeline.play();
+    }
+}
