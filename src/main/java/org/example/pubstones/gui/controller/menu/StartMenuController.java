b04e12623c5890814e491fc7ebceccf6733789ff
DustinScharf <d.scharf.2002@gmail.com>
2021-10-09 05:23:39 +0200

    Exchanged sound manager for the start menu

--- src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
+++ src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
@@ -15,34 +15,22 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StartMenuController extends BaseController implements Initializable {
-    private MediaPlayer mediaPlayer;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String path = getClass().getResource("/music/menu/1.mp3").toString();
-        Media media = new Media(path);
-        this.mediaPlayer = new MediaPlayer(media);
-
         // TODO restart after scene reload
-//        this.mediaPlayer.setVolume(0);
-//        this.mediaPlayer.play();
-//        Timeline timeline = new Timeline(
-//                new KeyFrame(Duration.seconds(3),
-//                        new KeyValue(this.mediaPlayer.volumeProperty(), 1)));
-//        timeline.play();
-        this.musicManager.setMusic("/music/menu/1.mp3");
-        this.musicManager.playMusic(3);
+        super.musicManager.setMusic("/music/menu/1.mp3");
+        super.musicManager.playMusic(3);
     }
 
     @FXML
     public void playButtonClicked(ActionEvent actionEvent) {
-        this.mediaPlayer.stop();
+        super.musicManager.stopMusic(1);
         super.sceneManager.switchScene("/gui/fxml/game/Game.fxml");
     }
 
     @FXML
     public void exitButtonClicked(ActionEvent actionEvent) {
-        this.mediaPlayer.stop();
+        super.musicManager.stopMusic(1);
         super.sceneManager.getStage().close();
     }
 }
