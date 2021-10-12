ca5557a5f93762b11785ab428fab45ae4fb305bb
DustinScharf <d.scharf.2002@gmail.com>
2021-10-12 04:29:15 +0200

    Added commented out code to set background from code

--- src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
+++ src/main/java/org/example/pubstones/gui/controller/menu/StartMenuController.java
@@ -5,14 +5,32 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import org.example.pubstones.gui.controller.BaseController;
 
+import java.net.URL;
+
 public class StartMenuController extends BaseController {
+    @FXML
+    AnchorPane anchorPane;
+
     @FXML
     Slider volumeSlider;
 
     @Override
     public void init() {
+//        URL imageUrl = getClass().getResource("/gui/images/background/start_menu_night.jpg");
+//        Image image = new Image(imageUrl.toString(), 800, 600, false, true); // TODO null check
+//        BackgroundImage backgroundImage = new BackgroundImage(
+//                image,
+//                BackgroundRepeat.NO_REPEAT,
+//                BackgroundRepeat.NO_REPEAT,
+//                BackgroundPosition.CENTER,
+//                BackgroundSize.DEFAULT
+//        );
+//        anchorPane.setBackground(new Background(backgroundImage));
+
         super.getManager().getMusicManager().setMusic("/music/menu/1.mp3");
         super.getManager().getMusicManager().setVolume(this.volumeSlider.getValue());
 
