f92d9e1c7a22db29dc0927ad5b46b3ebf3056c97
DustinScharf <d.scharf.2002@gmail.com>
2021-10-15 22:05:14 +0200

    Added getter and setter to UserSettings class

--- src/main/java/org/example/pubstones/gui/UserSettings.java
+++ src/main/java/org/example/pubstones/gui/UserSettings.java
@@ -9,4 +9,24 @@ public class UserSettings {
     public UserSettings(String settingsName) {
         this.settingsName = settingsName;
     }
+
+    public String getSettingsName() {
+        return settingsName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
 }
