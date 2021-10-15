2ad681f12c4c91f7d14edb97a81e7aec973af229
DustinScharf <d.scharf.2002@gmail.com>
2021-10-15 22:18:21 +0200

    Created basic user settings for test purpose

--- src/main/java/org/example/pubstones/gui/launcher/PubStonesApp.java
+++ src/main/java/org/example/pubstones/gui/launcher/PubStonesApp.java
@@ -2,6 +2,7 @@ package org.example.pubstones.gui.launcher;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.example.pubstones.gui.UserSettings;
 import org.example.pubstones.gui.manager.Manager;
 
 public class PubStonesApp extends Application {
@@ -14,6 +15,10 @@ public class PubStonesApp extends Application {
         Manager manager = new Manager(primaryStage);
 
 //        manager.getSceneManager().getStage().setFullScreen(true);
+
+        UserSettings userSettings = new UserSettings("settings1");
+        userSettings.setVolume(0.25);
+        manager.applyUserSettings(userSettings);
 
         manager.getSceneManager().switchScene("/gui/fxml/menu/StartMenu.fxml");
 
