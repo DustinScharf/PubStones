5f390f04fc0f2573908c25c5ff96f2a51f27d0d4
DustinScharf <d.scharf.2002@gmail.com>
2021-10-15 22:08:38 +0200

    Created applyUserSettings methode

--- src/main/java/org/example/pubstones/gui/manager/Manager.java
+++ src/main/java/org/example/pubstones/gui/manager/Manager.java
@@ -1,6 +1,7 @@
 package org.example.pubstones.gui.manager;
 
 import javafx.stage.Stage;
+import org.example.pubstones.gui.UserSettings;
 
 public class Manager {
     private final SceneManager sceneManager;
@@ -17,5 +18,9 @@ public class Manager {
 
     public MusicManager getMusicManager() {
         return musicManager;
+    }
+
+    public void applyUserSettings(UserSettings userSettings) {
+
     }
 }
