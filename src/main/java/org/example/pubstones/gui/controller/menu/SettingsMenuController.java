192828b6af6ac3db53110db23db98a6f15077b94
DustinScharf <d.scharf.2002@gmail.com>
2021-10-16 03:05:53 +0200

    Created SettingsMenuController.java

---
+++ src/main/java/org/example/pubstones/gui/controller/menu/SettingsMenuController.java
@@ -0,0 +1,7 @@
+package org.example.pubstones.gui.controller.menu;
+
+import org.example.pubstones.gui.controller.BaseController;
+
+public class SettingsMenuController extends BaseController {
+
+}
