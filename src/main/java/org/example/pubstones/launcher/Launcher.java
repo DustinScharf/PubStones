90d977a7dad3fcc4d618c6ae0a10e9c2a6a508f6
DustinScharf <d.scharf.2002@gmail.com>
2021-10-19 15:14:27 +0200

    Added javadoc to gui launcher

--- src/main/java/org/example/pubstones/launcher/Launcher.java
+++ src/main/java/org/example/pubstones/launcher/Launcher.java
@@ -2,6 +2,9 @@ package org.example.pubstones.launcher;
 
 import org.example.pubstones.gui.launcher.PubStonesApp;
 
+/**
+ * This class launches the gui
+ */
 public class Launcher {
     public static void main(String[] args) {
         PubStonesApp.main(args);
