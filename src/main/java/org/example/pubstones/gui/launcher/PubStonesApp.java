bea745a1c9527a5415a89a92a71a34f8d982b090
DustinScharf <d.scharf.2002@gmail.com>
2021-11-08 02:42:21 +0100

    Added comment on final / constant window title

--- src/main/java/org/example/pubstones/gui/launcher/PubStonesApp.java
+++ src/main/java/org/example/pubstones/gui/launcher/PubStonesApp.java
@@ -29,7 +29,7 @@ public class PubStonesApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // sets the title of the window corresponding to the game title
+        // sets the title of the window corresponding to the game title (yet hardcoded)
         primaryStage.setTitle("PubStones");
         // TODO null check
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/gui/icons/main_icon.png")));
